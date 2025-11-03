bf16f164ac81c7ad7a3f70255527c751dc0b6714
Ariana0-0 <thais.alomto@gmail.com>
2025-11-03 15:19:19 -0500

    Agrego métodos y atributos a la clase Veterinario y actualizo diagrama UML

--- g1_ReinoAnimal/src/pkVetSystem/pkhumano/Veterinario.java
+++ g1_ReinoAnimal/src/pkVetSystem/pkhumano/Veterinario.java
@@ -1,5 +1,49 @@
 package pkVetSystem.pkhumano;
 
-public class Veterinario {
+import pkVetSystem.pkReinoViviente.pkAnimal.AnimalVertebrado;
+
+public class Veterinario extends Persona {
+    public Veterinario() {
+        super();
+    }
+    public Veterinario(String cedula, String nombre, String apellido) {
+      super(cedula, nombre, apellido);
+   }
+    public boolean registarAnimal(AnimalVertebrado animal) {
+        if (animal != null) {
+            System.out.println("Animal registrado: " + animal.getNombre());
+            return true;
+        }
+        return false;
+    }
+
+    public boolean atenderAnimal(AnimalVertebrado animal) {
+          if (animal != null) {
+                System.out.println("Atendiendo al animal: " + animal.getNombre());
+                return true;
+          }
+          return false;
+    }
     
+    public String diagnosticarAnimal (AnimalVertebrado animal) {
+        return "El animal " + animal.getNombre() + " está enfermo.";
+    }
+
+    public String darTratamiento (AnimalVertebrado animal) {
+        return "Se debe dar una pasatilla al dia durante una semana a: " + animal.getNombre() + ".";
+    }
+
+    public String clasificarAnimal (AnimalVertebrado animal) {
+        return "El animal " + animal.getNombre() + " pertenece a la clase: ";
+    }
+
+    public String generarReporte (AnimalVertebrado animal) {
+        return "Reporte del animal: " + animal.getNombre() + "\n" +
+               "Clase: \n" +
+               "Diagnóstico: " + diagnosticarAnimal(animal) + "\n" +
+               "Tratamiento: " + darTratamiento(animal);
+    }
+
+
 }
+
